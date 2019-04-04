96f699a2102d48c72ce45fbdf50412bfcf156437
Anurag Patil <aspanurag@gmail.com>
2019-04-04 06:39:05 -0400

    Pics|Event Handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,13 +3,19 @@ import React from "react";
 class SearchBar extends React.Component {
   //   state: {};
 
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
+    //   this.onInputChange() - This will be called automatically when a component is rendered.
+    // Hence we only use it as 'this.onInputChange' which makes it a callback function and will be invoked only when its required.
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input className="" type="text" />
+            <input className="" type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
