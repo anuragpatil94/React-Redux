e032a4f54d7d94626f315bd51b330c5623a56591
Anurag Patil <aspanurag@gmail.com>
2019-04-04 06:26:04 -0400

    New App|Pics|Adding SearchBar Frontend

---
+++ pics/README.md
@@ -0,0 +1,24 @@
+# PICS
+
+- A simple app to display images
+
+## App Scope
+
+- App Structure - (i.e. Folders, organization)
+- Component Design
+- Handling User Requests
+- CSS
+- Fetch Data from external API
+
+## App Challenges
+
+- Get Search Term from User
+- Make API Request
+- Fetch Images and Show on Screen
+
+### Component Design
+
+- App
+- SearchBar
+- ImageList
+
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  //   state: {};
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input className="" type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
