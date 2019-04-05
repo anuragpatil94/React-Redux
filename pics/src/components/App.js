96f4db53694daf9baddf00e5f712ab2c589bad67
Anurag Patil <aspanurag@gmail.com>
2019-04-04 22:33:33 -0400

    Pics| Axios| connect to Unsplash API| axios get request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   // This function gets the change in state from child class to app class
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ee4e75a842cf172bdbd0e98e736755a0ff1afec1c5578f5870fd54d0c6491afc"
+      }
+    });
   }
 
   render() {
