12c5570b8f4594bcd43cec7c3813a29293c0f14b
Anurag Patil <aspanurag@gmail.com>
2019-04-05 10:18:38 -0400

    Pics| axios| async-await| adding state to App Component|Binding Callbacks using Arrow function

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,22 +4,40 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
   // This function gets the change in state from child class to app class
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID ee4e75a842cf172bdbd0e98e736755a0ff1afec1c5578f5870fd54d0c6491afc"
       }
     });
-  }
+
+    this.setState({ images: response.data.results });
+
+    // Async Request
+    /* Promise
+    axios.get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID ee4e75a842cf172bdbd0e98e736755a0ff1afec1c5578f5870fd54d0c6491afc"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
+      */
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         {/* We Passed a function as props from App -> SearchBar */}
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
