485a1b569c7d636304053b598bb55a4f5a81138a
Anurag Patil <aspanurag@gmail.com>
2019-04-04 21:53:19 -0400

    pics| solving context issues

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,12 +3,25 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  // This Throws error because for whatever reason the this is not bind to the functions. A way to solve this is using the Arrow Function
+  /*
+    onFormSubmit(event) {
+      event.preventDefault();
+      console.log(this.state.term);
+    }
+  */
+  // Arrow function automatically binds all the value of 'this' for all the code inside the function
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
+
   render() {
     // this.onInputChange() - This will be called automatically when a component is rendered.
     // Hence we only use it as 'this.onInputChange' which makes it a callback function and will be invoked only when its required.
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
