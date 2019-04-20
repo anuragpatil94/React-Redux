2d5bbbe900b92b8690a63d0ed27cc2eba791e734
Anurag Patil <aspanurag@gmail.com>
2019-04-19 22:56:08 -0400

    Streams|Client| Record Google UserIds

--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { SIGN_IN, SIGN_OUT } from "../actions/types";
-
 const INITIAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 };
 
 // When application starts reducer is called to initialize it.
@@ -10,9 +10,9 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload };
     case SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
