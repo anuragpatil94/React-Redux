a68b0a16ee7f9670832aac989514bac29ccd09d2
Anurag Patil <aspanurag@gmail.com>
2019-04-25 15:45:39 -0400

    Streams| RTMP| Server Running

---
+++ streams/rtmpserver/index.js
@@ -0,0 +1,19 @@
+// Check Documentation in https://github.com/illuspas/Node-Media-Server
+const NodeMediaServer = require("node-media-server");
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: "*"
+  }
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
